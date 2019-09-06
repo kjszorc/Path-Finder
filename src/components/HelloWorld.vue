5206fc13b40a9eb3ee68efae7443d4b8a92b971e
kjszorc <kjszorc@yahoo.com>
2019-09-05 19:35:38 -0500

    package update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,18 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="square">
+      <div class="circle">
+      </div>
+    </div>
+    <div class="square">
+      <div class="circle">
+      </div>
+    </div>
+    <div class="square">
+      <div class="circle">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -88,7 +21,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Path Finder Vue.js App'
     }
   }
 }
@@ -109,5 +42,38 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.square {
+  height: 30px;
+  width: 30px;
+  background-color: lightgrey;
+  position: relative;
+
+}
+.circle {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  background-color: orange;
+  border-radius: 50%;
+  display: inline-block;
+  margin: auto;
+  animation: changeColor 2s;
+}
+
+@keyframes changeColor {
+    from {
+      height: 0%;
+      width: 0%;
+      top: 15px;
+      right: 15px;
+    }
+    to {
+      height: 100%;
+      width: 100%;
+      top: 0px;
+      right: 0px;
+    }
 }
 </style>
