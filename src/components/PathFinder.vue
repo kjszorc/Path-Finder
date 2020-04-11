0c9fe55b3670cc3904c1690215f34cba68c26ab9
kjszorc <kjszorc@yahoo.com>
2020-04-10 20:09:14 -0500

    Fix DFS, add settings for height and width

--- src/components/PathFinder.vue
+++ src/components/PathFinder.vue
@@ -1,40 +1,53 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button class="btn--start" @click="runClick">Run</button>
-    <button class="btn--start" @click="clearGridClick">Clear</button>
-     <!-- <div class="square">
-      <div class="circle">
+    <section class="app--settings">
+      <div class="dot-options">
+        <div class="dot-option">
+          <label>
+            Place Starting point:
+            <input type="radio" v-model="pointType" value="0"/>
+          </label>
+        </div>
+        <div class="dot-option">
+          <label>
+            Walls:
+            <input type="radio" v-model="pointType" value="1"/>
+          </label>
+        </div>
+        <div class="dot-option">
+          <label>
+            Place Ending point:
+            <input type="radio" v-model="pointType" value="2"/>
+          </label>
+        </div>
       </div>
-    </div> -->
-
-    <!-- {{grid}} -->
-    <section class="dot-options">
-      <div class="dot-option">
-        <label>
-          Place Starting point:
-          <input type="radio" v-model="pointType" value="0"/>
+      <div class="grid-options">
+        <label>Height:
+          <input type="number" max="30" v-model="height">
         </label>
-      </div>
-      <div class="dot-option">
-        <label>
-          Walls:
-          <input type="radio" v-model="pointType" value="1"/>
+        <label>Width:
+          <input type="number" v-model="width">
         </label>
+        <button class="btn--set" @click="resetSquare">Set</button>
       </div>
-      <div class="dot-option">
-        <label>
-          Place Ending point:
-          <input type="radio" v-model="pointType" value="2"/>
-        </label>
-      </div>
+      <label>Algorithim type:
+        <select>
+          <option value="DFS">Depth First Search</option>
+          <option value="BFS">Breath First Search</option>
+        </select>
+      </label>
     </section>
+    <button class="btn--start" v-if="!isDone" @click="runClick">Run</button>
+    <button class="btn--start" v-if="isDone" @click="reRunClick">ReRun</button>
+    <button class="btn--start" @click="clearGridClick">Clear</button>
     <table class="table-path" ref="table" v-if="loaded">
       <tbody>
         <tr v-for="(rowValue, row, index) in grid" :key="index" ref="row">
           <td v-for="(colValue, col, indexC) in rowValue" :key="indexC" class="square" ref="col" @click="squareClick(row, col, $event)">
             <div :class="[{'circle':  colValue.hasDot}, colValue.color]" :style="{ 'animation-duration': colValue.delay}">
-              {{row }}, {{col}}
+              <!-- {{row }},{{col}}, -->
+              {{colValue.delay}}
             </div>
           </td>
         </tr>
@@ -75,7 +88,7 @@ export default {
   name: 'PathFinder',
   data () {
     return {
-      msg: '',//'A small Vue.js App',
+      msg: 'A small Vue.js App',
       loaded: false,
       pointType: 0,
       height: 5,
@@ -91,6 +104,7 @@ export default {
         'col': null,
       },
       isDone: false,
+      steps: 10,
     }
   },
 
@@ -98,6 +112,9 @@ export default {
     this.loaded = true;
   },
   methods: {
+    resetSquare: function() {
+      this.$forceUpdate();
+    },
     squareClick: function (row, col) {
       if (this.pointType == start) {
         if (this.startDot.row != null)
@@ -107,7 +124,7 @@ export default {
         this.grid[row].splice(col, 1, {hasDot: true, color: color[this.pointType], type: 'start'})
       }
       else if (this.pointType == wall) {
-        if (this.grid[row][col][0])
+        if (this.grid[row][col].hasDot)
           this.clearCircle({row: row, col: col});
         else
           this.grid[row].splice(col, 1, {hasDot: true, color: color[this.pointType]})
@@ -121,14 +138,14 @@ export default {
       }
     },
     clearGridClick: function () {
-      this.grid = Array.apply(null, {length: 5}).map(x => 
-        (Array.apply(null, {length: 5}).map(x => Object.assign({ hasDot: false, color: null}) )))
+      this.grid = Array.apply(null, {length: this.height}).map(x => 
+        (Array.apply(null, {length: this.width}).map(x => Object.assign({ hasDot: false, color: null}) )))
       this.startDot.row = null
       this.endDot.row = null
       this.isDone = false;
     },
     clearCircle: function(dot) {
-      this.grid[dot.row].splice(dot.col, 1, { hasDot: false, color: null})
+      this.grid[dot.row].splice(dot.col, 1, { hasDot: false, color: null, delay: null})
     },
 
     runClick: function () {
@@ -142,42 +159,59 @@ export default {
       const row = this.startDot.row
       const col = this.startDot.col
       // const level = 0
+
+      this.backupTable = Object.assign(this.grid); // lets make a backup
+
       this.fillTable(row, col);
     },
+    reRunClick: function() {
+      // we will need to know the type of dots to distinguish walls from paths
+      this.grid = Object.assign(this.backupTable);
+      this.isDone = false;
+      // this.runClick();
+    },
     // level is not passed in by value? but by reference
-    fillTable: function(row, col, l = 0) {
+    fillTable: function(row, col, l = 1) {
       if (row >= 0 &&
-         row < this.width && 
-         col >= 0 &&
-         col < this.height &&
-         !this.isDone) {
+          row < this.width && 
+          col >= 0 &&
+          col < this.height &&
+          !this.isDone) {
         let level = l
         console.log('location: ', row, ' x ', col, ' Level: ', level)
         if (this.endDot.row == row && this.endDot.col == col) {
           console.log('FOUND IT!')
           this.isDone = true;
-          return true
+          return true;
         }
         
         if (row + 1 < this.width) {
           console.log('try 1')
-          if (this.setCircle(row + 1, col, level))
-            this.fillTable(row + 1, col, 1 + level)
+          if (this.setCircle(row + 1, col, level)) {
+            if (this.fillTable(row + 1, col, 1 + level))
+              return true;
+          }
         }
         if (col + 1 < this.height) {
           console.log('try 2')
-          if (this.setCircle(row, col + 1, level))
-            this.fillTable(row, col + 1, 1 + level)
+          if (this.setCircle(row, col + 1, level)) {
+            if (this.fillTable(row, col + 1, 1 + level))
+              return true;
+          }
         }
         if (row - 1 >= 0) {
           console.log('try 3')
-          if (this.setCircle(row - 1, col, level))
-            this.fillTable(row - 1, col, level + 1)
+          if (this.setCircle(row - 1, col, level)) {
+            if (this.fillTable(row - 1, col, level + 1))
+              return true;
+          }
         }
         if (col - 1 >= 0) {
           console.log('try 4')
-          if (this.setCircle(row, col - 1, level))
-            this.fillTable(row, col - 1, level + 1)
+          if (this.setCircle(row, col - 1, level)) {
+            if (this.fillTable(row, col - 1, level + 1))
+              return true;
+          }
         }
         return false
       }
@@ -199,6 +233,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#app {
+  background-color: darkslateblue;
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -212,6 +249,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+label {
+  padding: 5px;
 }
 
 .square {
@@ -258,20 +299,44 @@ a {
   border-radius: 10px;
 }
 
+.btn--set {
+  width: 130px;
+  padding: 1px;
+  margin: auto;
+  margin-right: 25%;
+  
+  font-size: 14px;
+  border-radius: 10px;
+}
+
 .table-path {
   margin: auto;
 }
 
+.app--settings {
+  max-width: 1080px;
+  margin: auto;
+  display: flex;
+  padding-bottom: 20px;
+}
 .dot-options {
   display: flex;
   flex-direction: column;
+  width: 33%;
 }
 .dot-option {
   /* margin: auto; */
 }
 .dot-option label {
-  /* float: right; */
+  float: right;
 }
+
+.grid-options {
+  display: flex;
+  flex-direction: column;
+  width: 33%;
+}
+
 @keyframes changeColor {
     from {
       height: 0%;
